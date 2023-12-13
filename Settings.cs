d40787d56221fef5b08ce8cc80a3463502ce726b
Allan JGS <148894628+allanjgs@users.noreply.github.com>
2023-12-12 21:47:03 -0300

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -2,6 +2,7 @@ namespace ApiAuthSaude
 {
     public static class Settings
     {
-        public static string Secret = "Ch@v3S3cR3t@n@d@s3gur@";
+        //alterado chave de segurança por conta da sha256
+        public static string Secret = "5e38b654b84089239b27fff2ab58d4831a65629d82c8facd2ff2cb0def73e32d";
     }
 }
